d42c38a6321130089eefa2b9d87ff3afcfe2491a
Sey-khb <79506235+Sey-khb@users.noreply.github.com>
2021-06-08 13:51:55 +0700

    update route

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -14,7 +14,7 @@ class BankController extends Controller
      */
     public function index()
     {
-        //
+        echo "index";
     }
 
     /**
@@ -24,7 +24,7 @@ class BankController extends Controller
      */
     public function create()
     {
-        //
+        echo "create";
     }
 
     /**
