9c30ae5aba4f48bc1d3ed4d0b4b7da9caf7f2bcb
yemrahang <81398793+yemrahang@users.noreply.github.com>
2021-06-09 13:05:31 +0700

    update layout admin

--- resources/js/Backend/App.vue
+++ resources/js/Backend/App.vue
@@ -1,17 +1,38 @@
 <template>
     <div>
-        <Header/>
-            <router-view></router-view>
-        <Footer/>
+        <div class="wrapper">
+            <Header />
+            <!-- Main Wrapper -->
+            <div class="main-wrapper">
+                <Sidebar v-if="!reRender"/>
+                <router-view></router-view>
+            </div>
+            <Footer />
+        </div>
     </div>
 </template>
 <script>
 import Header from "./Include/Header.vue";
 import Footer from "./Include/Footer.vue";
+import Sidebar from "./Include/Sidebar.vue";
 export default {
-    components:{
+    data(){
+        return{
+            reRender: false
+        }
+    },
+    components: {
         Header,
-        Footer
+        Footer,
+        Sidebar
+    },
+    mounted(){
+        setTimeout(()=>{
+            this.reRender=true;
+            this.$nextTick(()=>{
+            this.reRender=false;
+        });
+        },5000);
     }
-}
+};
 </script>
---
+++ resources/js/Backend/Dashboard/router.js
@@ -0,0 +1,6 @@
+import Index from "./View/Index.js";
+export default{
+    router:[
+        {path:"/admin",component:Index},
+    ]
+}
