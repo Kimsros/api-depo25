3d8589fc7f09509efb12dff28fb0e33c52864adf
Sang Hang <84693219+khbSangHang@users.noreply.github.com>
2021-06-09 10:05:33 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/backend','backend\BackendController@index');
 Route::get('/backend/{any}/{id?}','backend\BackendController@index');
 Route::get('/','frontend\FrontendController@index');
+Route::get('/{any}/{title?}/{id?}', 'frontend\FrontendController@index');
 
 // Auth::routes();
 
