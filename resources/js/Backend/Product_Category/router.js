5bdf9da90e2bc9bf1add17ef97c29c8096a67857
Dunhvidou <vidou.dunh@khbmedia.asia>
2021-06-10 13:46:44 +0700

    update dashboard

--- resources/js/Backend/Product_Category/router.js
+++ resources/js/Backend/Product_Category/router.js
@@ -1,5 +1,9 @@
+import Add_category from "./View/Add_category.vue";
+import Index from "./View/Index.vue";
 export default{
     router:[
-        
+        {path:"/admin/add_category",component:Add_category},
+        {path:"/admin/list_category",component:Index},
+
     ]
 }
