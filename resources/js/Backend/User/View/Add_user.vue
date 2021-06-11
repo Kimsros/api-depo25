9ce871a10862354d7f3d553a3df99a0ca08246b0
Sey-khb <79506235+Sey-khb@users.noreply.github.com>
2021-06-11 14:54:10 +0700

    update

--- resources/js/Backend/User/View/Add_user.vue
+++ resources/js/Backend/User/View/Add_user.vue
@@ -43,57 +43,57 @@
                           <!-- End Form Group -->
                       </div>
 
-                      <div class="col-lg-6">
-                          <!-- Form Group -->
-                          <div class="form-group mb-20">
-                              <label for="password" class="mb-2 font-14 bold">Password</label>
-                              <input type="password" id="password" class="theme-input-style" placeholder="Password">
-                          </div>
-                          <!-- End Form Group -->
-                      </div>
+                        <div class="col-lg-6">
+                            <!-- Form Group -->
+                            <div class="form-group mb-20">
+                                <label for="password" class="mb-2 font-14 bold">Password</label>
+                                <input type="password" id="password" class="theme-input-style" placeholder="Password">
+                            </div>
+                            <!-- End Form Group -->
+                        </div>
 
-                      <div class="col-lg-6">
-                          <!-- Form Group -->
-                          <div class="form-group mb-20">
-                              <label for="r_password" class="mb-2 font-14 bold">Phone</label>
-                              <input type="text" id="r_password" class="theme-input-style" placeholder="Password">
-                          </div>
-                          <!-- End Form Group -->
-                      </div>
+                        <div class="col-lg-6">
+                            <!-- Form Group -->
+                            <div class="form-group mb-20">
+                                <label for="r_password" class="mb-2 font-14 bold">Phone</label>
+                                <input type="text" id="r_password" class="theme-input-style" placeholder="Password">
+                            </div>
+                            <!-- End Form Group -->
+                        </div>
 
-                      <div class="col-lg-6">
-                          <!-- Form Group -->
-                          <div class="form-group mb-20">
-                              <label for="r_password" class="mb-2 font-14 bold">Role</label>
-                              <input type="text" id="r_password" class="theme-input-style" placeholder="Role">
-                          </div>
-                          <!-- End Form Group -->
-                      </div>
+                        <div class="col-lg-6">
+                            <!-- Form Group -->
+                            <div class="form-group mb-20">
+                                <label for="r_password" class="mb-2 font-14 bold">Role</label>
+                                <input type="text" id="r_password" class="theme-input-style" placeholder="Role">
+                            </div>
+                            <!-- End Form Group -->
+                        </div>
 
 
-                      <div class="col-lg-6">
-                          <!-- Form Group -->
-                          <div class="form-group mb-20">
-                              <label for="r_password" class="mb-2 font-14 bold">Pricing</label>
-                              <input type="text" id="r_password" class="theme-input-style" placeholder="Pricing">
-                          </div>
-                          <!-- End Form Group -->
-                      </div>
+                        <div class="col-lg-6">
+                            <!-- Form Group -->
+                            <div class="form-group mb-20">
+                                <label for="r_password" class="mb-2 font-14 bold">Pricing</label>
+                                <input type="text" id="r_password" class="theme-input-style" placeholder="Pricing">
+                            </div>
+                            <!-- End Form Group -->
+                        </div>
 
-                      <div class="col-lg-6">
-                          <!-- Form Group -->
-                          <div class="form-group mb-20">
-                              <label for="r_password" class="mb-2 font-14 bold">Bank</label>
-                              <input type="text" id="r_password" class="theme-input-style" placeholder="Bank">
-                          </div>
-                          <!-- End Form Group -->
-                      </div>
-                  </div>
+                        <div class="col-lg-6">
+                            <!-- Form Group -->
+                            <div class="form-group mb-20">
+                                <label for="r_password" class="mb-2 font-14 bold">Bank</label>
+                                <input type="text" id="r_password" class="theme-input-style" placeholder="Bank">
+                            </div>
+                            <!-- End Form Group -->
+                        </div>
+                    </div>
 
-                  <div class="d-flex align-items-center pt-4">
-                      <button type="submit" class="btn long mr-20">Register</button>
-                      <span class="font-12 d-block"><a href="login.html" class="bold">Log In</a>,If you already have an account.</span>
-                  </div>
+                    <div class="d-flex align-items-center pt-4">
+                        <button type="submit" class="btn long mr-20">Register</button>
+                        <span class="font-12 d-block"><a href="login.html" class="bold">Log In</a>,If you already have an account.</span>
+                    </div>
               </form>
           </div>
       </div>
