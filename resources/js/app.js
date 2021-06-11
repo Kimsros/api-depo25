8924f240b3008998ad370023881266ad6877f4a4
Dunhvidou <vidou.dunh@khbmedia.asia>
2021-06-11 10:27:33 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,8 +23,6 @@ Vue.component('app-backend', require('./Backend/App.vue').default);
 Vue.component('app-frontend', require('./Frontend/App.vue').default);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
